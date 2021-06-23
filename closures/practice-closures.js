661e61f0c1ec9dd6d60c13d1495284ab58ecb265
Logan Banning <loganboganb@gmail.com>
2021-06-22 21:18:43 -0600

    second save

--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -9,12 +9,18 @@
   */
   
   //CODE HERE
-
+  function myFunc() {
+    let myStr = 'super secret string';
+    function getSecretString() {
+      return myStr;
+    }
+    return getSecretString;
+  }
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
   //CODE HERE
-  
+  let secretString = myFunc()
   
   ////////////PROBLEM 2////////////
 
@@ -29,27 +35,39 @@
   */
   
   //CODE HERE
-
+  function lightSwitch(){
+    let isTheLightOn = false;
+    function flipTheSwitch(){
+      isTheLightOn = !isTheLightOn;
+      if(isTheLightOn === true) {
+        return 'The light is on';
+      } else if(isTheLightOn === false) {
+        return 'The light is off';
+      }
+    }
+    return flipTheSwitch;
+  }
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
   //CODE HERE
-
+  let kitchenSwitch = lightSwitch();
   
   //Invoke kitchenSwitch.
 
   //CODE HERE
-  
+  kitchenSwitch();
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
   //CODE HERE
-  
+  let bathroomSwitch = lightSwitch();
   
   //Invoke bathroomSwitch twice.
   
   //CODE HERE
-
+bathroomSwitch();
+bathroomSwitch();
   
   
   ////////////PROBLEM 3////////////
@@ -64,7 +82,23 @@
   */
 
   //CODE HERE
-
+  var plantTracker = function() {
+    var plant = 'fern';
+    var height = 12;
+    return {
+      readInfo: function() {
+        return `This is a ${plant} plant that is ${height} inches tall.`
+      } ,
+      waterPlant: function() {
+        height++
+        return height;
+      },
+      prunePlant: function() {
+        height--
+        return height;
+      }
+    }
+  }
 
   ////////////PROBLEM 4////////////
 
@@ -79,18 +113,33 @@
   */
 
   //CODE HERE
+  // var inventory = function() {
+  //   var products: [];
 
+  //   return{
+  //     readProducts: function() {
+  //       return products;
+  //     },
+  //     addToProducts: function(str) {
+  //       products.push(str);
+  //     },
+  //     // deleteFromProducts: function(str){
+  //     //   var index = products.indexOf(str);
+  //     //   products.splice(index, 1);
+  //     // }
+  //   }
+  // }
 
   /*
     Create a variable called 'shoes' whose value is the invocation of inventory.
   */
 
   //CODE HERE
-
+var shoes = inventory();
 
   /*
     Add an item to your shoes array using the addToProducts function
   */
 
   //CODE HERE
-
+addToProducts('hat');
